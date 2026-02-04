4d18ad9fba2c564bfbffe88859ed4afaf516c0f4
Vinicius Dornelas <vinicius.s.dornelas@gmail.com>
2026-02-04 10:53:34 -0300

    feat: add route guards

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/auth/Login.vue')
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/Home.vue'),
+    meta: { auth: true }
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes
+})
+
+router.beforeEach((to) => {
+  const auth = useAuthStore()
+
+  if (to.meta.auth && !auth.isAuthenticated) {
+    return { name: 'login' }
+  }
+
+  if (to.name === 'login' && auth.isAuthenticated) {
+    return { name: 'home' }
+  }
 })
 
 export default router
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
+const auth = useAuthStore()
+const router = useRouter()
+
+function login() {
+  auth.loginFake()
+  router.push('/')
+}
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+    <button @click="login">Entrar</button>
+  </div>
+</template>
